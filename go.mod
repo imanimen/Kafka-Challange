ad6123b13eea9ade3e0e6c7ab5b9f9771480aea9
iman <imanimen81@gmail.com>
2024-05-22 16:35:40 +0330

    update: updated the sdk and producer code

--- go.mod
+++ go.mod
@@ -2,9 +2,13 @@ module github.com/imanimen/kafka-challange
 
 go 1.21.4
 
+require github.com/segmentio/kafka-go v0.4.47
+
 require (
-	github.com/confluentinc/confluent-kafka-go v1.9.2 // indirect
 	github.com/klauspost/compress v1.15.9 // indirect
 	github.com/pierrec/lz4/v4 v4.1.15 // indirect
-	github.com/segmentio/kafka-go v0.4.47 // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.1.2 // indirect
+	github.com/xdg-go/stringprep v1.0.4 // indirect
+	golang.org/x/text v0.13.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,18 +1,74 @@
 package main
 
-import "github.com/segmentio/kafka-go"
+import (
+	"context"
+	"crypto/tls"
+	"encoding/json"
+	"fmt"
+	"github.com/segmentio/kafka-go"
+	"github.com/segmentio/kafka-go/sasl/scram"
+	"time"
+)
 
 type Message struct {
-	Text  string `json:"text"`
+	Text  string `json:"test"`
 	State string `json:"state"`
 }
 
 func main() {
-	// kafka configuration
+	// configure the kafka
+	mechanism, err := scram.Mechanism(scram.SHA256,
+		"Z3VpZGVkLWdhemVsbGUtMTE3ODckNB1tfbKadjljFIv71Hm05KJEC1-IzwvmiCM",
+		"OTQxNmExZTUtNTliYS00MjU5LTkzNGUtYTcyYTJlMjg5MGZh")
+	if err != nil {
+		fmt.Println("Error creating SASL mechanism:", err)
+		return
+	}
 	w := &kafka.Writer{
 		Addr:  kafka.TCP("guided-gazelle-11787-us1-kafka.upstash.io:9092"),
-		Topic: "kafka-1",
+		Topic: "kafka-2",
+		Transport: &kafka.Transport{
+			SASL: mechanism,
+			TLS:  &tls.Config{},
+		},
+		BatchSize:  10,
+		BatchBytes: int64(100 * time.Millisecond),
 	}
 
 	// define states
+	states := []string{"failed", "completed", "in_progress"}
+
+	// create messages
+	messages := make([]Message, 1000)
+	for i := 0; i < 10; i++ {
+		state := states[i%len(states)]
+		message := Message{
+			Text:  fmt.Sprintf("message %d - state: %s", i, state),
+			State: state,
+		}
+		messages[i] = message
+	}
+
+	// encode messages to JSON bytes
+	var data []kafka.Message
+	for _, message := range messages {
+		msgBytes, err := json.Marshal(message)
+		if err != nil {
+			fmt.Println("error marshalling message:", err)
+			continue
+		}
+		data = append(data, kafka.Message{Value: msgBytes})
+	}
+
+	// write messages in batches
+	err = w.WriteMessages(context.Background(), data...)
+	if err != nil {
+		fmt.Println("Error writing messages to Kafka:", err)
+		return
+	}
+	fmt.Println("Successfully produced messages to Kafka")
+
+	if err := w.Close(); err != nil {
+		fmt.Println("Error closing connection:", err)
+	}
 }
